2c3c50362577004b3e2dddb06cb63692962095e9
ambi hidalgo <ambio.pk@gmail.com>
2023-04-21 21:43:10 -0700

    Add Context for database

---
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelterApi.Models
+{
+  public class AnimalShelterApiContext : DbContext
+  {
+    public DbSet<Animal> Animals {get;set;}
+
+    public AnimalShelterApiContext(DbContextOptions<AnimalShelterApiContext> options ) : base(options)
+    {
+
+    }
+
+    
+  }
+}
