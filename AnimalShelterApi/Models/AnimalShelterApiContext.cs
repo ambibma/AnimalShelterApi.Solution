9a6772558552b247596264b28826da92af7658f0
ambi hidalgo <ambio.pk@gmail.com>
2023-04-21 21:54:30 -0700

    Add seed to Context

--- AnimalShelterApi/Models/AnimalShelterApiContext.cs
+++ AnimalShelterApi/Models/AnimalShelterApiContext.cs
@@ -15,11 +15,11 @@ namespace AnimalShelterApi.Models
     {
       builder.Entity<Animal>()
               .HasData(
-                new Animal {AnimalId = 1, AnimalName = "Sparkles",  AnimalSex = "Female", AnimalType = "Cat", AnimalWeight = 17.5, AnimalNote = "Loves Yarn a bit too much"},
-                new Animal {AnimalId = 2, AnimalName = "Lizzy",  AnimalSex = "Male", AnimalType = "Cat", AnimalWeight = 17.5, AnimalNote = "Loves Yarn a bit too much"},
-                new Animal {AnimalId = 3, AnimalName = "Sparkles",  AnimalSex = "Female", AnimalType = "Cat", AnimalWeight = 17.5, AnimalNote = "Loves Yarn a bit too much"},
-                new Animal {AnimalId = 4, AnimalName = "Sparkles",  AnimalSex = "Female", AnimalType = "Cat", AnimalWeight = 17.5, AnimalNote = "Loves Yarn a bit too much"},
-                new Animal {AnimalId = 5, AnimalName = "Sparkles", AnimalSex = "Female", AnimalType = "Cat", AnimalWeight = 17.5, AnimalNote = "Loves Yarn a bit too much"},
+                new Animal {AnimalId = 1, Name = "Sparkles",  Sex = "Female", Type = "Cat", Weight = 17.5, Note = "Loves Yarn a bit too much"},
+                new Animal {AnimalId = 2, Name = "Lizzy",  Sex = "Male", Type = "Iguana", Weight = 5, Note = "Loves baking in the sun"},
+                new Animal {AnimalId = 3, Name = "Daisy",  Sex = "Female", Type = "Dog", Weight = 50.5, Note = "Has no thoughts"},
+                new Animal {AnimalId = 4, Name = "Hank",  Sex = "male", Type = "Chicken", Weight = 6.5, Note = "Needs anger management"},
+                new Animal {AnimalId = 5, Name = "Beatrice", Sex = "Female", Type = "Cat", Weight = 40.5, Note = "Loves eating living her best life"}
                 
               );
     }
